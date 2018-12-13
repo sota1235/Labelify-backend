4b75cfae9db1a0630397693e6ad47a6ab1534aed
sota1235 <sota1235@gmail.com>
2018-12-13 15:26:01 +0900

    modify: using express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import resolvers from './resolvers';
 import typeDefs from './schema';
 
 const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+  console.log(`🚀  Server ready at localhohst:4000${server.graphqlPath}`);
 });
+
